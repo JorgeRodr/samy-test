ef7614266bcd652edd9d79cb402a3e156cf773ee
Jorge Rodriguez <jorge.rodriguez@boream.com>
2019-05-17 21:07:50 +0200

    Unit tests for like component

--- src/components/like/like.test.js
+++ src/components/like/like.test.js
@@ -5,12 +5,23 @@ import { Like } from "./like";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
+
+Enzyme.configure({ adapter: new Adapter() });
+
 library.add(faThumbsUp);
 
 describe("like component", () => {
-  it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Like />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  let wrapper;
+  let mockClick = jest.fn();
+
+  beforeEach(() => {
+    wrapper = shallow(<Like onClick={mockClick} />);
+  });
+  it("should emit event on click", () => {
+    wrapper.find(".gallery__icon-like").simulate("click");
+    expect(mockClick).toHaveBeenCalled();
   });
 });
